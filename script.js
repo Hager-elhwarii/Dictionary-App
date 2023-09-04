1b417b021ca5a7b4f4b538054a5a7c8129b2f2e1
hager-dot <hagerelhwarii6633@gmail.com>
2023-09-04 11:46:44 +0300

    Api Integration

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 const searchBtn = document.getElementById("search-btn");
 const result = document.getElementById("wrapper");
+const soundId = document.getElementById("sound");
+
 
 searchBtn.addEventListener("click", () => {
   let inputValue = document.getElementById("search-input").value;
@@ -8,25 +10,34 @@ searchBtn.addEventListener("click", () => {
   fetch(`${url}${inputValue}`)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data)
+      console.log(data);
       result.innerHTML = ` <div class="word-contanier">
     <h2>${inputValue}</h2>
-    <button>
+
+    <button  onclick="playSound()">
       <i class="fa-solid fa-volume-high"></i>
     </button>
   </div>
     
   <div class="details">
-    <p>${data[0].meanings[0].partOfSpeech}</p>
-    <p>${data[0].phonetics[1].text}</p>
+    <p>${data[0].meanings[0].partOfSpeech || ""}</p>
+    <p>${data[0].phonetics[1].text || ""}</p>
   </div>
 
   <div class="meaning">
-    <p class="word-meaning">${data[0].meanings[0].definitions[0].definition}</p>
-    <p class="word-example">${data[0]}</p>
+    <p class="word-meaning">${
+      data[0].meanings[0].definitions[0].definition || ""
+    }</p>
+    <p class="word-example">${
+      data[0].meanings[0].definitions[0].example || ""
+    }</p>
   </div>`;
+    soundId.setAttribute("src",`${data[0].phonetics[1].audio}`)
     })
-    .catch((error) => console.log(error));
+    .catch(() => (result.innerHTML = `<h3>Couldn't Find The Word!!</h3>`));
+});
 
-})
+function playSound() {
+    soundId.play();
+}
 
